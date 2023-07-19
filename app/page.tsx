9ef6ba78dce1ed9b68cd96ce1c09ecf993105a10
taiti09 <taitiabe1997@gmail.com>
2023-07-19 10:48:13 +0900

    完成

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import ArticleList from "./component/ArticleList";
 import { Heading } from "./common/components/index";
 
 async function getArticles() {
-  const res = await fetch("http://localhost:3000/api/articles");
+  const res = await fetch("http://localhost:3000/api/articles",{
+    cache: "no-store",
+  });
 
   // エラーハンドリングを行うことが推奨されている
   if (!res.ok) {
@@ -20,7 +22,7 @@ export default async function Home() {
   return (
     <div>
       <Heading as="h1" mb={4}>新着記事</Heading>
-      <ArticleList articles={articles}></ArticleList>
+      <ArticleList articles={articles} />
     </div>
   );
 }
